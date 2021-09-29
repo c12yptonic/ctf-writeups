23ffdd05e53df9e67f484c22a907c8beff477322
c12yptonic <cl2yp7on1c@protonmail.com>
2021-09-29 23:09:36 +0530

    Minor changes in gdb article and pre-commit script

--- hooks/add_modified_date.sh
+++ hooks/add_modified_date.sh
@@ -2,7 +2,7 @@
 # Contents of .git/hooks/pre-commit
 
 git diff --cached --name-status | grep "^M" | while read a b; do
-  cat $b | sed "/---.*/,/---.*/s/^last_modified_at:.*$/last_modified_at: $(date +"%d-%m-%Y %I:%M %p %z")/" > tmp_f
+  cat $b | sed "/---.*/,/---.*/s/^last_modified_date:.*$/last_modified_date: $(date +"%d-%m-%Y %I:%M %p %z")/" > tmp_f
   mv tmp_f $b
   git add $b
 done
